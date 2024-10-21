3c6685b331cf8b2ab89bb57c442cc332a1610438
vsnnnnn <christian.bomboi@student.pxl.be>
2024-10-21 14:11:52 +0200

    commit 1

---
+++ wpfapp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (NameTextBox.Text.Equals("admin") && PasswordTextBox.Text.Equals("admin"))
+            {
+                InfoTextBlock.Text = "U wordt ingelogd.";
+            }
+        }
+        private void LogOffButton_Click(object sender, RoutedEventArgs e)
+        {
+            InfoTextBlock.Text = "";
+        }
+    }
+}
